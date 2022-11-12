7855ca4adf3296aa42c3cf4c90630e4967967420
jjk <jedrzejk143@gmail.com>
2022-11-12 21:41:31 +0100

    fix cleanup_pods

--- scripts/shell/cleanup_pods.sh
+++ scripts/shell/cleanup_pods.sh
@@ -6,7 +6,7 @@ pods_no=$(kubectl get pods -n $namespace --no-headers | grep -iv "running" | wc 
 printf "$pods_no pods in $namespace will be deleted\n"
 for pod in $(kubectl get pods -n $namespace -o custom-columns=":metadata.name" --no-headers)
 do
-    if [ $(kubectl get pod $pod -n $namespace -o jsonpath="{.status.phase}") != "Start" ]; then
+    if [ $(kubectl get pod $pod -n $namespace -o jsonpath="{.status.phase}") != "Runnning" ]; then
         kubectl delete pod $pod -n $namespace;
     fi
 done
